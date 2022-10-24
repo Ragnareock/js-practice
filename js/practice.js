b5637903244e495467548b1e68185046101bbbd7
Andriy Lobash <andry.lobash@gmail.com>
2022-10-24 22:35:51 +0300

    Update

--- js/practice.js
+++ js/practice.js
@@ -1,1 +1,14 @@
+function add(a, b, c) {
+  return a + b + c;
+}
+add(2, 5, 8); // 15
+console.log(add(15, 27, 10));
+console.log(add(10, 20, 30));
+console.log(add(5, 10, 15));
 
+function add(a, b, c) {
+  console.log(`Addition result equals ${a + b + c}`);
+}
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
